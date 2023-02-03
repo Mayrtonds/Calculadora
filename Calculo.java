a8deb35a5dd29f95046e61f0e1286e4d96b5be51
mayrtonds <mayrton.adm@gmail.com>
2023-02-02 21:53:30 -0300

    Projeto calculadora

---
+++ Calculo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class  Calculo {
+
+public static void main(String[] args) {
+	
+	int n1;
+	int n2;
+	
+	String  operacao = "";
+	int control=0;
+	
+	Scanner entrada = new Scanner(System.in);
+	
+	System.out.print("Digite a operação desejada (+, -, *, /): ");
+	operacao = entrada.nextLine();
+
+}
