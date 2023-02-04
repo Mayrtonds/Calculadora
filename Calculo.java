70115fb1acf77757896c919cf13366b4ce0e6cda
Mayrtonds <Mayrton.adm@gmail.com>
2023-02-04 18:26:13 -0300

    Atividade atualizada

--- Calculo.java
+++ Calculo.java
@@ -7,11 +7,21 @@ public static void main(String[] args) {
 	int n2;
 	
 	String  operacao = "";
+	
 	int control=0;
 	
 	Scanner entrada = new Scanner(System.in);
 	
 	System.out.print("Digite a operação desejada (+, -, *, /): ");
+	
 	operacao = entrada.nextLine();
 
+	System.out.println("Digite um valor: ");
+
+	n1 = Integer.parseInt(entrada.nextLine());
+
+	System.out.println("Digite um valor: ");
+
+	n2 = Integer.parseInt(entrada.next());
+	}
 }
